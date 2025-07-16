ded693da76cf2f2e39a2a85e15707a36e022fa6a
Pedro Jesus <pa04052007@gmail.com>
2025-07-16 18:23:15 -0300

    desafio.py

--- desafio.py
+++ desafio.py
@@ -1,15 +1,20 @@
 limit = int(input("Digite o número de linhas: "))
 space = ""
 position = 0
+times = 0
 
 for x in range(0,limit,1):
     space += " "
     position += 1
     print(space, end="")
     print("#")
+    times += 1
     if position == 5:
-        while position != 1:
+        for x in range(0,5,1):
             space = space.replace(" ", "",1)
             position -=1
+            times += 1
             print(space, end="")
             print("#")
+    if times == limit:
+        break
